558c2eafc4588d0e96890efb4d9a9897ab1c7b03
ernest99 <vybrantalpha@gmail.com>
2022-07-10 11:21:54 +0000

    new commit

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -7,6 +7,7 @@ const Product = () => {
        <div>
         <h1>Product</h1>
         <Btn text="Product Button" className="btn"/>
+        <h2>This is the product page....</h2>
        </div>
     )
 }
