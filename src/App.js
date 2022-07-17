c7823f6b21010f74208b6fa096655df1eb41059f
ernest99 <vybrantalpha@gmail.com>
2022-07-17 10:03:07 +0000

    working on react routers

--- src/App.js
+++ src/App.js
@@ -4,36 +4,27 @@ import Home from './components/Home';
 import Product from './components/Product'
 import About from './components/About'
 import Service from './components/Service';
-import {Route,Routes} from 'react-router-dom'
-
+import {Route, Routes} from 'react-router-dom'
 
 function App() {
-   //write javascript here
+    //write javascript here
     return (
         <div className="App">
             <Navbar/>
-            
+
             <Routes>
-                <Route path='/' element={
-                <>
-                <Home/>
-                <Service/>
-                </>
-            }>
-      
-            </Route>
+                <Route path='/' element={<> <Home/> < Service /> < Product /> < About /> </>}></Route>
             </Routes>
             <Routes>
-                <Route path='/product' element={<Product/>}></Route>
+                <Route path='/product' element={<> <Home/> < Product /> </>}></Route>
             </Routes>
             <Routes>
-            <Route path='/about' element={<About/>}></Route> 
+                <Route path='/about' element={< About />}></Route>
             </Routes>
-          <Routes>
-            <Route path='/service' element={<Service/>}></Route>
-          </Routes>
-          
-           
+            <Routes>
+                <Route path='/service' element={< Service />}></Route>
+            </Routes>
+
         </div>
     );
 }
