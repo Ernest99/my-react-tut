fdca7f69da2f55186f7c91be4c8d2216799f8a53
vibrant_coder <74645353+Ernest99@users.noreply.github.com>
2022-07-24 09:44:48 +0000

    added a counter feature

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,36 @@ import React from 'react'
 import Btn from './Btn'
 import img1 from '../assets/logo-bcc.jpg'
 import NavLink from './NavLink'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
+
+const Person = ({name,age}) =>{
+  return(
+    <>
+    <h2>Name:{name} </h2>
+    <h2>Age: {age}</h2>
+    </>
+  )
+}
+
 const About = () => {
+const [counter, setCounter] = useState(0);
   return (
     <div>
         <h1>About</h1>
         <img src={img1} alt="" />
         <Btn text="About Me" className="btn"/>
         <NavLink/>
+        <div>
+          <Link to="/">Feature {'>'}<span className='space'>blog</span> </Link>
+        </div>
+        <Person name={'John'} age={20}/>
+        <div className='counter'>
+          <button onClick={()=> setCounter(counter - 1) }>-</button>
+          <h2>{counter}</h2>
+          <button onClick={() => setCounter(counter + 1)}>+</button>
+        </div>
     </div>
   )
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Btn from './Btn'
 import NavLink from './NavLink'
-
+import Person from './About'
 
 const Product = () => {
 
     return (
        <div>
         <h1>Product</h1>
+        <Person name='Max' age='20'/>
         <Btn text="Product Button" className="btn"/>
         <h2>This is the product page....</h2>
         <NavLink/>
